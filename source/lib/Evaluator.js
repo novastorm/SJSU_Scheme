1fdc70f54604aa7ede57fd8201dc18f8a31c4acc
Adland Lee <adland@4mfd.com>
2012-10-21 16:38:28 -0700

    add initial primitive function handling

--- source/lib/Evaluator.js
+++ source/lib/Evaluator.js
@@ -12,14 +12,18 @@ function dump (x) {
 };
 
 module.exports = {
-
+    _AList : null,
+    
     initialize : function () {
         this._AList = Object.create(List);
         this._AList.initialize();
+        var cons_add = List.cons(
+                List.element(Constant.SYMBOL, this._primitive[0].symbol),
+                List.element(Constant.PRIMATIVE, 0)
+            );
+        this._AList.push(cons_add);
     },
 
-    _AList : null,
-    
     alist : function () {
         return this._AList.head(); 
     },
@@ -39,11 +43,10 @@ module.exports = {
         var theResults = this.process(aExpr);
 //        dump (theResults);
         
-        if (theResults.type == Constant.CONS) {
+        if (theResults.type == Constant.CONS || theResults.type == Constant.NIL) {
             theReturnValue = this.display(theResults);
         }
         else {
-        
             theReturnValue = theResults;
         }
         
@@ -59,13 +62,13 @@ module.exports = {
 
     process : function (aExpr) {
         console.log('process');
-        var theCar 
+        var theCar;
         var theCdr;
+        var node;
         
         var theReturnValue;
         
         if (aExpr.type == Constant.CONS) { // a list
-            return aExpr;
             console.log('aExpr.type: ' + aExpr.type + ' <=> ' + Constant.CONS);
             theCar = aExpr.car;
             theCdr = aExpr.cdr;
@@ -78,8 +81,12 @@ module.exports = {
             if (theCar.type == Constant.SYMBOL) {
                 console.log('theCar.type: ' + theCar.type + ' <=> ' + Constant.SYMBOL);
                 if ( theCar.val == '+') {
+/*
                     console.log('cdar[' + theCdr.car.val + '] cddar[' + theCdr.cdr.car.val + ']');
                     return (theCdr.car.val + theCdr.cdr.car.val);
+*/
+                    node = this._AList.lookup(theCar.val);
+                    return this._primitive[node.cdr.val].operation(theCdr);
                 }
             }
             else if (theCar.type == Constant.LAMBDA) {
@@ -91,6 +98,7 @@ module.exports = {
         }
         else if (aExpr.type == Constant.SYMBOL){
             console.log('aExpr.type: ' + aExpr.type + ' <=> ' + Constant.SYMBOL);
+            return this._AList.lookup(aExpr.val);
             return ('aExpr.type: ' + aExpr.type + ' <=> ' + Constant.SYMBOL);
         }
         else { // an atom
@@ -119,19 +127,19 @@ module.exports = {
  ******************************************************************************/
  
     display : function (expr) {
-        dump(expr);
+//        dump(expr);
         if (expr.type == Constant.CONS) {
             if (expr.cdr.type == Constant.CONS || expr.cdr.type == Constant.NIL) {
-                console.log('LIST');
+//                console.log('display LIST');
                 return '(' + this.display(expr.car) + this.display_extended(expr.cdr, '');
             }
             else {
-                console.log('IMPROPER LIST');
+//                console.log('display IMPROPER LIST');
                 return '(' + this.display(expr.car) + ' . ' + this.display(expr.cdr) + ')';
             }
         }
         else {
-            console.log('ATOM');
+//            console.log('ATOM');
             return expr.val;
         }
     },
@@ -144,19 +152,54 @@ module.exports = {
  ******************************************************************************/
  
     display_extended : function (expr, string) {
-        dump(expr);
+//        dump(expr);
         if (expr.type == Constant.CONS) {
-            console.log('more CONS');
+//            console.log('display extended CONS');
             return this.display_extended(expr.cdr, string + ' ' + this.display(expr.car));
         }
         else if (expr.type == Constant.NIL) {
-            console.log('more NIL');
+//            console.log('display extended NIL');
             return string + ')';
         }
         else {
-            console.log('more ATOM');
+//            console.log('display extended ATOM');
             return string + ' ' + expr.val;
         }
-    }
+    },
+    
+
+/******************************************************************************
+ 
+    add
+ 
+ ******************************************************************************/
+ 
+    _primitive : [
+        {
+            symbol : '+',
+            operation : function (sexpr) {
+                console.log('add');
+                var result = 0;
+                while (sexpr.type != Constant.NIL) {
+                    result += sexpr.car.val;
+                    sexpr = sexpr.cdr;
+                }
+                return result;
+            }
+        },
+        {
+            symbol : '-',
+            operation : function (sexpr) {
+                var result = 0;
+                while (sexpr.type != Constant.NIL) {
+                    result -= sexpr.car.val;
+                    sexpr = sexpr.cdr;
+                }
+                return result;
+            }
+        }
+    ],
+        
+ 
 };
 
